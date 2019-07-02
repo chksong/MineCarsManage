9eda91603242ab97db7c00c42e89a7714ab498c7
chksong <schkui@qq.com>
2019-07-02 18:43:04 +0800

    修改添加记录的界面

--- adddayworkdialog.cpp
+++ adddayworkdialog.cpp
@@ -20,8 +20,9 @@ AddDayWorkDialog::~AddDayWorkDialog()
 
 
 void AddDayWorkDialog::on_PB_TEST_clicked()
-{
+{ 
+	//测试时间输出
     auto ret = ui->dateEdit->date().toString("yyyy-MM-dd  ddd") ;
-    auto ret2 = ui->dateTimeEdit->dateTime().toString("yyyy-MM-dd hh:mm:ss ddd") ;
-    auto ret3 = ui->calendarWidget->selectedDate().toString("yyyy-MM-dd ddd") ;
+    //auto ret2 = ui->dateTimeEdit->dateTime().toString("yyyy-MM-dd AP hh:mm:ss ddd") ;
+    //auto ret3 = ui->calendarWidget->selectedDate().toString("yyyy-MM-dd ddd") ;
 }
